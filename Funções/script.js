cf6eaa098891846ea0c5c783875daab38fa788a4
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-31 14:19:36 -0300

    Função de cores

--- Funções/script.js
+++ Funções/script.js
@@ -2,12 +2,21 @@
     return lado * lado;
     
 }
-console.log(areaQuadrado(4)) */
-function imc(peso, altura) {
+/* console.log(areaQuadrado(4)) */
+/* function imc(peso, altura) {
     const imc = peso / (altura ** 2);
     return imc;
 }
 console.log(imc(80, 1.80));
-console.log(imc(80, 1.70));
-
+console.log(imc(80, 1.70)); */ 
+function cofFavorita(cor) {
+    if (cor === 'azul') {
+        return 'Você gosta do céu';
+    } else if (cor === 'verde') {
+        return 'Você gosta de mato';
+    } else {
+        return 'Você gosta de nada';
+    }
+}
+console.log(cofFavorita( ));
 
