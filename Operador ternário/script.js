a0465c457b446f1649ebd4dd9c12a195b9fa3722
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-03 08:34:15 -0300

    Operador Ternário

---
+++ Operador ternário/script.js
@@ -0,0 +1,6 @@
+// o operador ternário serve para verificar uma cvondição  e depende da condição
+// var nome = (condição) ? se for verdadeiro "escreve isso " : senão "escreve isso";
+// condição ? True : False
+var idade = 25;
+var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
+console.log(podeBeber)
