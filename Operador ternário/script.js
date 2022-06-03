306c505b8d59ea897bfaf898fb74e45ac8aad71a
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-03 08:59:47 -0300

    Operador com ternario e diversos

--- Operador ternário/script.js
+++ Operador ternário/script.js
@@ -1,6 +1,21 @@
 // o operador ternário serve para verificar uma cvondição  e depende da condição
-// var nome = (condição) ? se for verdadeiro "escreve isso " : senão "escreve isso";
+// var nome = (condição) ? se for verdadeiro "escreve isso,pode ser colocado numero, string e qualquer tipo de " : senão "escreve isso";
 // condição ? True : False
 var idade = 25;
-var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
+var possuDiabetes = true;
+//var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
+var podeBeber = (idade >= 18 && possuDiabetes) ? 'Pode beber': 'Não pode beber';
 console.log(podeBeber)
+// quando não e necessario abri e fechas chaves
+
+
+var scrool = 1000;
+scrool += 500;
+console.log(scrool);
+
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito;
+darCredito = (possuiCarro && possuiCasa);
+darCredito = (possuiCarro && possuiCasa) ? 'Dar credito' : 'Negar credito';
+console.log(darCredito);
